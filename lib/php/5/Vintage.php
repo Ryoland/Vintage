fbb715946f25c3a2032f759ad792c1c9142d96cd
Ryo Yanagida <r.yanagida@adominisutoretanoMacBook-Pro.local>
2025-04-02 16:12:27 +0900

    Updated.

--- lib/php/5/Vintage.php
+++ lib/php/5/Vintage.php
@@ -1,66 +1,73 @@
 <?php
 
-    namespace {
+  namespace {
 
-        final class Vintage {
+    final class Vintage {
 
-            private static $INIT_DATETIME = null;
-            private static $DPS_EXTENSION = array(
-                '/opt/local/lib/php/pear',
-                '/usr/share/pear'
-            );
+      private static $INIT_DATETIME = null;
+      private static $DPS_EXTENSION = array(
+        '/opt/local/lib/php/pear',
+        '/usr/share/pear'
+      );
 
-            final private static function autoload($class) {
+      private static function autoload($class) {
+        if (preg_match('/^Vintage\\\/', $class)) {
+          require_once(
+            $_SERVER['VTG_ROOT'] .
+            '/pro/Vintage/lib/php/5/' .
+            str_replace('\\', '/', $class) .
+            '.php'
+          );
+        }
+        else {
+          @include_once(str_replace('\\', '/', $class) . '.php');
+        }
+      }
 
-                if (preg_match('/^Vintage\\\/', $class)) {
-                    require_once(
-                        $_SERVER['VTG_ROOT'] .
-                        '/pro/Vintage/lib/php/5/' .
-                        str_replace('\\', '/', $class) .
-                        '.php'
-                    );
-                } else {
-                    // コメントアウトするとメール送れなくなる。
-                    @include_once(str_replace('\\', '/', $class) . '.php');
-                }
-            }
+      public static function initialize() {
 
-            final public static function initialize() {
+        date_default_timezone_set('Asia/Tokyo');
+        spl_autoload_register('\Vintage::autoload');
 
-                date_default_timezone_set('Asia/Tokyo');
-                spl_autoload_register('\Vintage::autoload');
+        self::initialize_conf();
 
-                self::initialize_conf();
+        foreach (self::$DPS_EXTENSION as $DP_EXTENSION) {
+          if (is_dir($DP_EXTENSION)) {
+            ini_set('extension_dir', $DP_EXTENSION);
+          }
+        }
+      }
 
-                foreach (self::$DPS_EXTENSION as $DP_EXTENSION) {
-                    if (is_dir($DP_EXTENSION)) {
-                        ini_set('extension_dir', $DP_EXTENSION);
-                    }
-                }
-            }
+      private static function initialize_conf() {
 
-            final private static function initialize_conf() {
-                $INIT_DATETIME = explode(',', date('Y,m,d,H,i,s'));
-                for ($i = 0; $i < count($INIT_DATETIME); $i++) {
-                    $INIT_DATETIME[$i] = (integer) $INIT_DATETIME[$i];
-                }
-                self::$INIT_DATETIME = $INIT_DATETIME;
-            }
+        $INIT_DATETIME = explode(',', date('Y,m,d,H,i,s'));
 
-            final public static function init_datetime($mode = 0) {
-                if ($mode == 0) {
-                    return self::$INIT_DATETIME;
-                } elseif ($mode == 1) {
-                    $format = '%04d%02d%02d%02d%02d%02d';
-                    return vsprintf($format, self::$INIT_DATETIME);
-                } elseif ($mode == 2) {
-                    $format = '%04d/%02d/%02d %02d:%02d:%02d';
-                    return vsprintf($format, self::$INIT_DATETIME);
-                } else { throw new \Exception(); }
-            }
+        for ($i = 0; $i < count($INIT_DATETIME); $i++) {
+          $INIT_DATETIME[$i] = (integer) $INIT_DATETIME[$i];
         }
 
-        \Vintage::initialize();
+        self::$INIT_DATETIME = $INIT_DATETIME;
+      }
+
+      public static function init_datetime($mode = 0) {
+        if ($mode == 0) {
+          return self::$INIT_DATETIME;
+        }
+        else if ($mode == 1) {
+          $format = '%04d%02d%02d%02d%02d%02d';
+          return vsprintf($format, self::$INIT_DATETIME);
+        }
+        else if ($mode == 2) {
+          $format = '%04d/%02d/%02d %02d:%02d:%02d';
+          return vsprintf($format, self::$INIT_DATETIME);
+        }
+        else {
+          throw new \Exception();
+        }
+      }
     }
+
+    \Vintage::initialize();
+  }
 
 ?>
--- lib/php/5/Vintage/C/Util/Process.php
+++ lib/php/5/Vintage/C/Util/Process.php
@@ -1,148 +1,348 @@
 <?php
 
-    namespace Vintage\C\Util {
+  namespace Vintage\C\Util {
 
-        use \Vintage\C\PHP\VString as VString;
+    /*** Process */
+    final class Process {
 
-        final class Process extends \Vintage\A\Lib {
+      /*** Start */
+      public static function start(array $a):array {
 
-            final public static function start(array $process = []) {
+        $x = ['status' => false];
+        $r = [];
 
-                $name = isset($process['name']) ? $process['name'] : null;
+        // Check (Arguments)
+        $r = self::check_a((array_merge($a, ['method' => 'start'])));
 
-                $d = [];
-
-                if (!isset($name)) {
-                    $d['message'] = 'No name.';
-                    goto NG;
-                }
-
-                $file = self::path($process);
-
-                if (file_exists($file)) {
-
-                    $contents = file_get_contents($file);
-                    $datetime = substr($contents, 0, 14);
-
-                    if (time() - strtotime($datetime) > 60 * 60) {
-
-                        if (!unlink($file)) {
-                            $d['message'] = 'Failed to unlink.';
-                            goto NG;
-                        }
-                    }
-                    else {
-                        $d['message'] = 'Process already exists.';
-                        goto NG;
-                    }
-                }
-
-                $id = date('YmdHis') . '' . mt_rand();
-
-                if (!file_put_contents($file, $id)){
-                    $d['message'] = 'Failed to write file.';
-                    goto NG;
-                }
-
-
-
-
-////
-        $process['id'] = $id;
-////
-
-
-
-
-                $d['id'] = $id;
-
-                $d['message'] = 'Succeeded to write file.';
-                goto OK;
-
-                NG :
-                $d['status'] = false;
-                goto FIN;
-
-                OK :
-                $d['status'] = true;
-                goto FIN;
-
-                FIN :
-
-                return $d;
-            }
-
-            final public static function finish(array $a = []) {
-
-                $id = isset($a['id']) ? $a['id'] : null;
-
-                $d = [];
-
-                if (!isset($id)) {
-                    $d['message'] = 'No id.';
-                    goto NG;
-                }
-
-                $path = self::path($a);
-
-                if (!file_exists($path)) {
-                    $d['message'] = 'Process is not running.';
-                    goto OK;
-                }
-
-                $contents = file_get_contents($path);
-
-                if ($contents == $id) {
-                    if (!unlink($path)) {
-                        $d['message'] = 'Failed to finish.';
-                        goto NG;
-                    }
-                }
-                else {
-                    $d['message'] = 'ID is wrong.';
-                    goto NG;
-                }
-
-                $d['message'] = 'Finished.';
-                goto OK;
-
-                NG :
-                $d['status'] = false;
-                goto FIN;
-
-                OK :
-                $d['status'] = true;
-                goto FIN;
-
-                FIN :
-
-                return $d;
-            }
-
-            final public static function running(array $a = []) {
-                $path = self::path($a);
-                if (file_exists($path)) {
-                    return true;
-                }
-                return false;
-            }
-
-            final private static function path(array $a) {
-
-                $project = isset($a['project']) ? $a['project'] : null;
-                $name    = isset($a['name'])    ? $a['name']    : null;
-
-                $dir  = $_SERVER['VTG_ROOT'] . '/tmp';
-
-                if (!is_dir($dir)) {
-                    mkdir($dir, 0777, true);
-                }
-
-                $path = $dir . '/' . $project . '.' . $name . '.process';
-
-                return $path;
-            }
+        if (!$r['status']) {
+          $x['message'] = 'Arguments wrong.';
+          $x['line']    = __LINE__;
+          goto NG;
         }
+
+        $a = $r['a'];
+        $r = [];
+        ///
+
+        // $fp_free, $sequence_free, $count
+        $fp_free       = null;
+        $sequence_free = null;
+
+        $count = [
+          'dead'    => 0,
+          'free'    => $a['processes_max'],
+          'killed'  => 0,
+          'running' => 0
+        ];
+
+        for ($i = 1; $i <= $a['processes_max']; $i++) {
+
+          $fp = self::fp(array_merge($a, ['sequence' => $i]));
+
+          if (!$fp) {
+            $x['message'] = 'Failed to get file path.';
+            $x['line']    = __LINE__;
+            goto NG;
+          }
+          else if (file_exists($fp)) {
+
+            $contents = file_get_contents($fp);
+            $process  = unserialize($contents);
+
+            if (posix_kill($process['id'], 0)) {
+              $count['free']--;
+              $count['running']++;
+            }
+            else if (!unlink($fp)) {
+              $x['message'] = 'Failed to unlink dead file.';
+              $x['line']    = __LINE__;
+              goto NG;
+            }
+            else {
+
+              $fp_free       = $fp_free       ?? $fp;
+              $sequence_free = $sequence_free ?? $i;
+
+              $count['dead']++;
+            }
+          }
+          else {
+            $fp_free       = $fp_free       ?? $fp;
+            $sequence_free = $sequence_free ?? $i;
+          }
+        }
+
+        if (!$count['free']) {
+          $x['message'] = 'No free processes.';
+          $x['line']    = __LINE__;
+          goto NG;
+        }
+        ///
+
+        // Start
+        $pid = getmypid();
+
+        if (!is_numeric($pid)) {
+          $x['message'] = 'Failed to get PID.';
+          $x['line']    = __LINE__;
+          goto NG;
+        }
+
+        $process = [
+          'id'         => $pid,
+          'project'    => $a['project'],
+          'name'       => $a['name'],
+          'number'     => $a['number'],
+          'sequence'   => $sequence_free,
+          'file'       => $fp_free,
+          'started_at' => date('Y-m-d H:i:s')
+        ];
+
+        $contents = serialize($process);
+
+        if (!file_put_contents($fp_free, $contents)) {
+          $x['message'] = 'Failed to start.';
+          $x['line']    = __LINE__;
+          goto NG;
+        }
+
+        $count['free']--;
+        $count['running']++;
+        ///
+
+        OK :
+
+        $x['status']  = true;
+        $x['message'] = $x['message'] ?? 'ok';
+        $x['line']    = $x['line']    ?? __LINE__;
+
+        $x['id']      = $pid; // Backward Compatibility
+        $x['count']   = $count;
+        $x['process'] = $process;
+
+        return $x;
+
+        NG :
+
+        $x['trace']   = $r['trace'] ?? [];
+        $x['trace'][] = [
+          'message' => $x['message'],
+          'line'    => $x['line'],
+          'class'   => __CLASS__,
+          'method'  => __METHOD__
+        ];
+
+        return $x;
+      }
+
+      /*** Finish */
+      public static function finish(array $a):array {
+
+        $x = ['status' => false];
+        $r = [];
+
+        // Check (Arguments)
+        $r = self::check_a(array_merge($a, [
+          'method' => 'finish'
+        ]));
+
+        if (!$r['status']) {
+          $x['message'] = 'Arguments wrong.';
+          $x['line']    = __LINE__;
+          goto NG;
+        }
+
+        $a = $r['a'];
+        $r = [];
+        ///
+
+        // Check (File)
+        $fp = self::fp($a);
+
+        if (!$fp) {
+          $x['message'] = 'Failed to get file path.';
+          $x['line']    = __LINE__;
+          goto NG;
+        }
+        else if (!file_exists($fp)) {
+          $x['message'] = 'File not found.';
+          $x['line']    = __LINE__;
+          goto NG;
+        }
+
+        $contents = file_get_contents($fp);
+        $process  = unserialize($contents);
+
+        if ($a['id'] != $process['id']) {
+          $x['message'] = 'IDs not matched.';
+          $x['line']    = __LINE__;
+          goto NG;
+        }
+        ///
+
+        // Finish
+        if (!unlink($fp)) {
+          $m            = 'Failed to unlink %s.';
+          $x['message'] = sprintf($m, $fp);
+          $x['line']    = __LINE__;
+          goto NG;
+        }
+
+        OK :
+
+        $x['status']  = true;
+        $x['message'] = $x['message'] ?? 'ok';
+        $x['line']    = $x['line']    ?? __LINE__;
+
+        $process['finish_at'] = date('Y-m-d H:i:s');
+
+        $x['process'] = $process;
+
+        return $x;
+
+        NG :
+
+        $x['trace']   = $r['trace'] ?? [];
+        $x['trace'][] = [
+          'message' => $x['message'],
+          'line'    => $x['line'],
+          'class'   => __CLASS__,
+          'method'  => __METHOD__
+        ];
+
+        return $x;
+      }
+
+      /*** File Path */
+      private static function fp(array $a) {
+
+        $fp = null;
+
+        $dp_root = $_SERVER['VTG_ROOT'] ?? '';
+        $dp      = $dp_root . '/tmp';
+
+        if (!is_dir($dp)) {
+          if (!mkdir($dp, 0755, true)) {
+            goto FIN;
+          }
+        }
+
+        $fp = sprintf(
+          '%s/%s.%s.%d.%d.process',
+          $dp,
+          $a['project'],
+          $a['name'],
+          $a['number'],
+          $a['sequence']
+        );
+
+        FIN : return $fp;
+      }
+
+      /*** Check (Arguments) */
+      private static function check_a(array $a) {
+
+        $x = ['status' => false];
+        $r = [];
+
+        // Common
+        {
+          $a['method']  = $a['method']  ?? null;
+          $a['project'] = $a['project'] ?? 'vintage';
+          $a['name']    = $a['name']    ?? null;
+          $a['number']  = $a['number']  ?? 1;
+
+          if (!strlen($a['project'])) {
+            $x['message'] = '$a["project"] wrong.';
+            $x['line']    = __LINE__;
+            goto NG;
+          }
+          else if (!strlen($a['name'])) {
+            $x['message'] = '$a["name"] wrong.';
+            $x['line']    = __LINE__;
+            goto NG;
+          }
+          else if (!preg_match('/^[1-9][0-9]*$/', $a['number'])) {
+            $x['message'] = '$a["number"] wrong.';
+            $x['line']    = __LINE__;
+            goto NG;
+          }
+
+          $a['number'] = (int) $a['number'];
+        }
+
+        // Start
+        if ($a['method'] == 'start') {
+
+          $a['processes_max'] = $a['processes_max'] ?? 1;
+
+          if (!preg_match('/^[1-9][0-9]*$/', $a['processes_max'])) {
+            $x['message'] = '$a["processes_max"] wrong.';
+            $x['line']    = __LINE__;
+            goto NG;
+          }
+
+          $a['processes_max'] = (int) $a['processes_max'];
+        }
+        // Finish
+        else if ($a['method'] == 'finish') {
+
+          $a['id']       = $a['id']       ?? null;
+          $a['sequence'] = $a['sequence'] ?? null;
+
+          if (!$a['id']) {
+            $x['message'] = '$a["id"] wrong.';
+            $x['line']    = __LINE__;
+            goto NG;
+          }
+          else if (!preg_match('/^[1-9][0-9]*$/', $a['id'])) {
+            $x['message'] = '$a["id"] wrong.';
+            $x['line']    = __LINE__;
+            goto NG;
+          }
+          else if (!$a['sequence']) {
+            $x['message'] = '$a["sequence"] wrong.';
+            $x['line']    = __LINE__;
+            goto NG;
+          }
+          else if (!preg_match('/^[1-9][0-9]*$/', $a['sequence'])) {
+            $x['message'] = '$a["sequence"] wrong.';
+            $x['line']    = __LINE__;
+            goto NG;
+          }
+
+          $a['id']       = (int) $a['id'];
+          $a['sequence'] = (int) $a['sequence'];
+        }
+        else {
+          $x['message'] = '$a["method"] wrong.';
+          $x['line']    = __LINE__;
+          goto NG;
+        }
+
+        OK :
+
+        $x['status']  = true;
+        $x['message'] = $x['message'] ?? 'ok';
+        $x['line']    = $x['line']    ?? __LINE__;
+
+        $x['a'] = $a;
+
+        return $x;
+
+        NG :
+
+        $x['trace']   = $r['trace'] ?? [];
+        $x['trace'][] = [
+          'message' => $x['message'],
+          'line'    => $x['line'],
+          'class'   => __CLASS__,
+          'method'  => __METHOD__
+        ];
+
+        return $x;
+      }
     }
+  }
 
 ?>
